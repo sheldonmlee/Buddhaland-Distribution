fe705a3c54d9ab4c301ab1f909db4adb449a9754
Sheldon Lee <sheldon@sheldonlee.com>
2024-08-21 08:30:20 +0800

    Randomly permute combinations until unique found
    This method randomly selects from available attribute names (the count
    is not 0), until max tries is reached.
    
    It is pretty fast, but not perfect because having the exact frequencies
    for 1000 makes it become stuck at around 800 combinations. Afterwards,
    the probability is too low to generate a new combination from non 0
    attributes.

--- distribution.py
+++ distribution.py
@@ -13,68 +13,76 @@ parser.add_argument('--all-possible', action='store_true',
 
 args = parser.parse_args()
 
-heads = {
-        "Plain": 100,
-        "Mathura": 100,
-        "Gandara": 100,
-        "Tang": 100,
-        "Han": 100,
-        "Gupta": 100,
-        "Jewel": 100,
-        "Tibetan": 100,
-        "Thai": 100,
+mudras = {
+        "Dhyana": 200,
+        "Bhumisparsha": 110,
+        "Dharmchakra": 60,
+        "Varada": 120,
+        "Abhaya": 150,
+        "Anajali": 200,
+        "Vitarka": 130,
+        "Combo": 30,
+        }
+
+hairstyles = {
+        "Sixteen Kingdoms": 50,
+        "Mathura": 30,
+        "Gandhara": 150,
+        "Tang": 70,
+        "338": 20,
+        "Gupta": 200,
+        "LotusSutra": 130,
+        "Himalayas": 160,
+        "Sukkothai": 90,
         "Khmer": 100,
         }
 
 materials = {
-        "Aurora": 25,
-        "Rainbow": 25,
-        "Fire": 25,
-        "Ocean": 25,
-        "Gold": 150,
-        "Forest": 150,
-        "Lotus": 150,
+        "Aurora": 80,
+        "Rainbow": 40,
+        "Fire": 100,
+        "Ocean": 90,
+        "Gold": 210,
+        "Forest": 80,
+        "Lotus": 70,
         "Porelain": 150,
-        "Earth": 150,
-        "Silicon": 150,
+        "Earth": 80,
+        "Silicon": 100,
         }
 
-hands = {
-        "Combo": 90,
-        "Dyana": 130,
-        "Bhumisparsa": 130,
-        "Dharmachakra": 130,
-        "Varada": 130,
-        "Abaya": 130,
-        "Anjali": 130,
-        "Vitarka": 130,
-        }
-
-background_gems = {
-        "Diamond": 100,
-        "Ruby": 180,
-        "Emerald": 180,
-        "Lapis Lazuli": 180,
-        "Amber": 180,
-        "Taaffeite": 180,
+background_urnas = {
+        "Diamond": 150,
+        "Ruby": 210,
+        "Emerald": 120,
+        "Lapis Lazuli": 170,
+        "Amber": 260,
+        "Taaffeite": 90,
         }
 
 
-def pop_item(items):
-    keys = list(items.keys())
-    random.shuffle(keys)
-    key = keys[0]
-    key_index = 0
+def pop_item(items, key):
+    if key is None:
+        return None
 
-    while not items[key]:
-        key_index += 1
-        if key_index > len(keys)-1:
-            return None
-
-        key = keys[key_index]
+    if not items[key]:
+        return None
 
     items[key] -= 1
     return key
+
+
+def get_random_key(items):
+    keys = list(items.keys())
+    _keys = list(items.keys())
+
+    for key in _keys:
+        if not items[key]:
+            keys.remove(key)
+
+    if len(keys) == 0:
+        return None
+    random.shuffle(keys)
+    return keys[0]
 
 
 def get_attribute_rarity(attribute, attribute_distribution):
@@ -86,36 +94,69 @@ def get_attribute_rarity(attribute, attribute_distribution):
     return rarity
 
 
-def get_total_rarity(head, material, hand, background_gem):
-    return get_attribute_rarity(head, heads) * \
+def get_total_rarity(mudra, material, hairstyle, background_urna):
+    return get_attribute_rarity(mudra, mudras) * \
             get_attribute_rarity(material, materials) * \
-            get_attribute_rarity(hand, hands) * \
-            get_attribute_rarity(background_gem, background_gems)
+            get_attribute_rarity(hairstyle, hairstyles) * \
+            get_attribute_rarity(background_urna, background_urnas)
+
+
+def multiply_items(items, x):
+    for key in items:
+        items[key] *= x
 
 
 def generate_objects(file_handle):
-    _heads = copy.deepcopy(heads)
+    existing_combos = {}
+    _mudras = copy.deepcopy(mudras)
+    _hairstyles = copy.deepcopy(hairstyles)
     _materials = copy.deepcopy(materials)
-    _hands = copy.deepcopy(hands)
-    _background_gems = copy.deepcopy(background_gems)
+    _background_urnas = copy.deepcopy(background_urnas)
 
-    while True:
-        head = pop_item(_heads)
-        material = pop_item(_materials)
-        hand = pop_item(_hands)
-        background_gem = pop_item(_background_gems)
+    coefficient = 1
+    multiply_items(_mudras, coefficient)
+    multiply_items(_hairstyles, coefficient)
+    multiply_items(_materials, coefficient)
+    multiply_items(_background_urnas, coefficient)
 
-        if head is None or material is None or hand is None or background_gem is None:
+    maxtries = 100000
+    tries = 0
+    count = 0
+    while tries < maxtries:
+        mudra_key = get_random_key(_mudras)
+        material_key = get_random_key(_materials)
+        hairstyle_key = get_random_key(_hairstyles)
+        background_urna_key = get_random_key(_background_urnas)
+        combo = f'{mudra_key}{material_key}{hairstyle_key}{background_urna_key}'
+        if combo in existing_combos:
+            tries += 1
+            continue
+
+        mudra = pop_item(_mudras, mudra_key)
+        material = pop_item(_materials, material_key)
+        hairstyle = pop_item(_hairstyles, hairstyle_key)
+        background_urna = pop_item(_background_urnas, background_urna_key)
+
+        if mudra is None or material is None or hairstyle is None or background_urna is None:
+            continue
+
+        total_rarity = get_total_rarity(mudra, material, hairstyle, background_urna)
+
+        out_line = f'{mudra},{hairstyle},{material},{background_urna},{total_rarity*100:.4f}\n'
+        file_handle.write(out_line)
+
+        count += 1
+        if count == 1000:
             break
 
-        total_rarity = get_total_rarity(head, material, hand, background_gem)
+        existing_combos[combo] = True
 
-        out_line = f'{head},{material},{hand},{background_gem},{total_rarity*100:.4f}\n'
-        file_handle.write(out_line)
+    if tries == maxtries:
+        print("max tries")
 
 
 def generate_header(file_handle):
-    file_handle.write('Head,Material,Hand,Background Gem,Rarity %\n')
+    file_handle.write('Mudra,Hairstyle,Material,Background & Urnas,Rarity %\n')
 
 
 def generate_file(filename):
@@ -130,13 +171,13 @@ def generate_all_possible(filename):
     csv = open(filename, 'w')
     generate_header(csv)
 
-    for head in heads:
-        for material in materials:
-            for hand in hands:
-                for background_gem in background_gems:
-                    total_rarity = get_total_rarity(head, material, hand, background_gem)
+    for mudra in mudras:
+        for hairstyle in hairstyles:
+            for material in materials:
+                for background_urna in background_urnas:
+                    total_rarity = get_total_rarity(mudra, material, hairstyle, background_urna)
 
-                    out_line = f'{head},{material},{hand},{background_gem},{total_rarity*100:.4f}\n'
+                    out_line = f'{mudra},{material},{hairstyle},{background_urna},{total_rarity*100:.4f}\n'
                     csv.write(out_line)
     csv.close()
     print(f'written to {filename}')
